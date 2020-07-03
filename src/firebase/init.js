b47fcd4db1becedbf37c0065f27033579064e1e8
pickle-radish <tkdansg0222@gmail.com>
2020-07-03 15:29:35 +0900

    rebuild project in firebase

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -3,13 +3,13 @@ import firebase from 'firebase'
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.VUE_APP_FIREBASE_KEY,
-  authDomain: "easy-name-card.firebaseapp.com",
-  databaseURL: "https://easy-name-card.firebaseio.com",
-  projectId: "easy-name-card",
-  storageBucket: "easy-name-card.appspot.com",
-  messagingSenderId: "427420481851",
-  appId: "1:427420481851:web:06794cd20cb8bf54111f1e",
-  measurementId: "G-87V9PWK3F4"
+  authDomain: "easy-name-card-47671.firebaseapp.com",
+  databaseURL: "https://easy-name-card-47671.firebaseio.com",
+  projectId: "easy-name-card-47671",
+  storageBucket: "easy-name-card-47671.appspot.com",
+  messagingSenderId: "854642965580",
+  appId: "1:854642965580:web:3ef00bda820b29dfecc8e9",
+  measurementId: "G-FKFSNHQDD9"
 };
 // Initialize Firebase
 export default firebase.initializeApp(firebaseConfig);
