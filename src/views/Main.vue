7987ba47c73ce28293599167ff26030f473398e3
pickle_radish <tkdansg0222@gmail.com>
2020-07-22 00:59:03 +0900

    add how to button in main page

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,7 +1,11 @@
 <template>
     <div id="mainDiv">
-        <router-link :to="{name:`SelectTemp`}">
-            <v-btn x-large rounded color="primary" dark>Get Start!</v-btn>
+        <router-link :to="{name:`Manual`}" tag="span" class="mainBtn">
+            <v-btn x-large rounded width=150>How to?</v-btn>
+            <!-- <v-btn x-large rounded width=150 style="text-transform: none">How to?</v-btn> -->
+        </router-link>
+        <router-link :to="{name:`SelectTemp`}" tag="span" class="mainBtn">
+            <v-btn x-large rounded color="primary" width=150>Get Start!</v-btn>
         </router-link>
     </div>
 </template>
@@ -21,11 +25,15 @@ export default {
 
 <style>
     #mainDiv{
-        width:100%;
+        width:100%; 
         height:100%;
 
         display: flex;
         justify-content: center;
         align-items: center;
     }
+    .mainBtn{
+        margin:10px;
+    }
+    
 </style>
